6a7d871901391ebdf191e00644b362ecdb61d222
Nick Wade <nick@nwade.com>
2017-04-12 18:23:54 -0600

    Add initial aliases script

---
+++ aliases.sh
@@ -0,0 +1,11 @@
+alias brewit="brew update && brew upgrade && brew doctor"
+
+alias gw="./gradlew"
+
+alias g="git"
+alias gs="git status"
+alias gpr="git pull -r"
+alias gc="git commit"
+alias ga="git add"
+
+
